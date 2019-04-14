0ea5b0fe63b227dc4240597ab9127e55ba58471c
thomas shellberg <thomas@shellberg.com>
2019-04-14 09:34:01 +0200

    add logic to pull and sort contacts on birthday screen

--- screens/BirthdayScreen.js
+++ screens/BirthdayScreen.js
@@ -1,64 +1,61 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { List, ListItem, Text } from 'native-base'
+import { ScrollView, StyleSheet, FlatList } from 'react-native';
+import { ListItem, Text } from 'native-base'
+import moment from 'moment'
+import _ from 'lodash'
+import firebase from '@firebase/app'
+import '@firebase/auth'
+import '@firebase/database'
 
 export default class BirthdayScreen extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: []
+    }
+    db = firebase.database()
+  }
+
   static navigationOptions = {
     title: 'Birthdays',
-  };
+  }
+
+  componentWillMount() {
+    const userId = firebase.auth().currentUser.uid;
+    contacts = db.ref(`users/${userId}/contacts`).orderByChild('birthday')
+    contacts.on('value', (snapshot) => {
+      this.updateContacts(snapshot.val());
+    });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.contacts)
+  }
+
+  getFormattedBirthday = (date) => {
+    return moment(date).format("MMMM Do YYYY")
+  }
+
+  renderListItem(contact) {
+    return (
+      <ListItem itemDivider>
+        <Text>{`${contact.firstName} ${contact.lastName} - ${this.getFormattedBirthday(contact.birthday)}`}</Text>
+      </ListItem>     
+    )
+  }
+
+  updateContacts(contacts) {
+    this.setState({ contacts: _.values(contacts) })
+  }
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
     return (
       <ScrollView style={styles.container}>
-        <List>
-          <ListItem itemDivider>
-            <Text>January</Text>
-          </ListItem>                    
-          <ListItem>
-            <Text>Aaron Bennet - January 13</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Ali Connors - January 22</Text>
-          </ListItem>
-          <ListItem itemDivider>
-            <Text>February</Text>
-          </ListItem>  
-          <ListItem>
-            <Text>Bradley Horowitz - February 15</Text>
-          </ListItem>
-          <ListItem itemDivider>
-            <Text>March</Text>
-          </ListItem>  
-          <ListItem>
-            <Text>Chuck Chuckerson - March 3</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Chula Chuckerson - March 7</Text>
-          </ListItem>
-          <ListItem itemDivider>
-            <Text>April</Text>
-          </ListItem>  
-          <ListItem>
-            <Text>Darrel Darrelson</Text>
-          </ListItem>
-          <ListItem>
-            <Text>Debbie Darrelson</Text>
-          </ListItem>
-          <ListItem itemDivider>
-            <Text>May</Text>
-          </ListItem>  
-          <ListItem>
-            <Text>Eric Ericksson</Text>
-          </ListItem>
-          <ListItem itemDivider>
-            <Text>June</Text>
-          </ListItem>  
-          <ListItem>
-            <Text>Frank Franksson</Text>
-          </ListItem>
-        </List>
+        <FlatList
+          data={this.state.contacts}
+          renderItem = { ({item}) => this.renderListItem(item) }
+        />
       </ScrollView>
     )
   }
