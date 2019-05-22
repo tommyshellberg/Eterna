d1867a86725be0869cf7b9386114195636f849e3
thomas shellberg <thomas@shellberg.com>
2019-05-22 20:20:43 +0200

    remove data folder

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,6 @@
 import * as functions from 'firebase-functions';
 const sanitizeHtml = require('sanitize-html');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 const sanitizeData = ( data:any ) => {
     return sanitizeHtml(data, {
         allowedTags: [],
